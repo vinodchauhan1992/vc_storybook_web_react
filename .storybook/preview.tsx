f6dc84ab30a6dc00a0c07505c0ec95ee9e0cbe86
vinodchauhan1992 <69452412+vinodchauhan1992@users.noreply.github.com>
2025-05-09 22:23:56 +0530

    theme done in half way

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "../src/index.css"; // Import your global styles
+import "../src/styles/global-styles.css";
 import type { Preview } from "@storybook/react";
 import {
   Title,
@@ -11,52 +12,86 @@ import {
 } from "@storybook/blocks";
 import { ThemeProvider } from "@mui/material";
 import { withThemeByDataAttribute } from "@storybook/addon-themes";
-import { customLightTheme, customDarkTheme } from "../src/themes";
 
 const preview: Preview = {
-  tags: ["autodocs"],
-  decorators: [
-    withThemeByDataAttribute({
-      themes: {
-        light: "light",
-        dark: "dark",
-      },
-      defaultTheme: "light",
-      attributeName: "data-mode",
-    }),
-    (Story, context) => (
-      <ThemeProvider
-        theme={
-          context.globals.theme === "dark" ? customDarkTheme : customLightTheme
-        }
-      >
-        <Story />
-      </ThemeProvider>
-    ),
-  ],
-  globals: {
-    theme: "light", // Initial theme
-  },
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+      sort: "requiredFirst",
     },
+
     docs: {
-      page: () => (
-        <>
-          <Title />
-          <Subtitle />
-          <Description />
-          <Primary />
-          <Controls />
-          <Stories />
-        </>
-      ),
+      controls: {
+        sort: "requiredFirst",
+      },
+    },
+
+    backgrounds: {
+      default: "Auto",
+      values: [
+        { name: "Auto", value: "var(--vc-color-bg-canvas)" },
+        {
+          name: "Transparent",
+          value:
+            "repeat conic-gradient(#666 90deg, #aaa 90deg 180deg, #666 180deg 270deg, #aaa 270deg) 0 0 / 32px 32px",
+        },
+        { name: "White", value: "var(--vc-color-bg-white-canvas)" },
+        { name: "Black", value: "var(--vc-color-bg-black-canvas)" },
+      ],
+    },
+
+    options: {
+      storySort: {
+        method: "alphabetical",
+        order: ["docs", ["Overview"], "Foundations", "Components"],
+      },
     },
   },
+
+  globalTypes: {
+    colorMode: {
+      description: `VCComponent theme's color mode option`,
+      defaultValue: "Light",
+      toolbar: {
+        title: "Color Mode",
+        items: ["Light", "Dark"],
+        dynamicTitle: true,
+      },
+    },
+    subBrand: {
+      description: `VCComponent theme's sub brand option`,
+      defaultValue: "VCComponent",
+      toolbar: {
+        title: "Sub brand",
+        items: ["VCComponent", "VCComponent Save"],
+        dynamicTitle: true,
+      },
+    },
+  },
+
+  decorators: [
+    (story, context) => {
+      document.body.classList.toggle(
+        "vc-theme-main--light",
+        context.globals.subBrand === "VCComponent" &&
+          context.globals.colorMode === "Light"
+      );
+      document.body.classList.toggle(
+        "vc-theme-main--dark",
+        context.globals.subBrand === "VCComponent" &&
+          context.globals.colorMode === "Dark"
+      );
+      document.body.classList.toggle(
+        "vc-theme-save--light",
+        context.globals.subBrand === "VCComponent Save"
+      );
+
+      return story();
+    },
+  ],
 };
 
 export default preview;
